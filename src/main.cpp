156149bc2e8d50e238af84735bcec9d057942b1b
Danielgranton <danielgranton634@gmail.com>
2026-07-01 02:38:56 +0300

    updated studioos-media

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include <string>
+#include <grpcpp/grpcpp.h>
+#include "server/MediaServer.hpp"
 
-using namespace std;
+using grpc::Server;
+using grpc::ServerBuilder;
 
 int main() {
 
-    cout << "hello" << endl;
+    MediaServer service;
+
+    ServerBuilder builder;
+
+    builder.AddListeningPort(
+        "0.0.0.0:50051",
+        grpc::InsecureServerCredentials());
+
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+
+    std::cout << "StudioOS Media Service running on port 50051\n";
+
+    server->Wait();
 
     return 0;
 }
